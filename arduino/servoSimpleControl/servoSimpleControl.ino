11bc99354a7a38acd44a88ebdfa3a7ad60f7c70f
Mazin Chabayta <mazin.chabayta@gmail.com>
2018-11-21 23:33:50 -0500

    Kate edits

--- arduino/servoSimpleControl/servoSimpleControl.ino
+++ arduino/servoSimpleControl/servoSimpleControl.ino
@@ -1,17 +1,63 @@
 
+//#include <Adafruit_PWMServoDriver.h>
+
+#include <Servo.h>
 #include <Wire.h>
-#include <Adafruit_PWMServoDriver.h>
 
-uint8_t servonum1= 0;
-uint8_t servonum2 = 0;
+Servo myservo1;
+Servo myservo2;
 
-void setup() 
-{ 
-  servonum1.attach(9);
-  servonum2.write(170);  // set servo to mid-point
+void setup ()
+{
+  myservo1.attach(9);
+myservo1.writeMicroseconds (3000);
+  //myservo1.write (180);
 
   myservo2.attach(10);
-  myservo2.write(10);
-} 
+myservo2.writeMicroseconds (1000);
+  //myservo2.write (180);
+}
 
-void loop() {} 
+void loop () {}
+
+
+
+
+
+
+
+
+//int servopin1 = 9; //pin that the servo is attached to
+//int moveRate = 10;
+//int minAngle = 0;
+//int maxAngle = 180;
+//int moveIncrement = 1
+//int servoAngle1;
+//
+//long lastTimeYouMoved1;
+//
+//
+//Servo servo1 //creating servo object
+//
+//
+//void setup() 
+//{ 
+//  myservo1.attach(myservo1);
+//  servoAngle1= minAngle;
+//
+//} 
+//
+//void loop() 
+//if (millis ()-lastTimeYouMoved1>= moveRate)
+//{ 
+//  servo1.write (servoAngle1);
+//  servoAngle1 +- moveIncrement;
+//}
+//
+//if (servoAngle1<= minAngle || servoAngle1 >= maxAngle)
+//{
+//  moveIncrement = -moveIncrement;
+//  } 
+//  lastTimeYouMoved1 = millis ();
+//  }
+//  }
